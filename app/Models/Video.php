fe776df6fa06f58c5830623f45090f4404871881
Atif <atif@monotote.com>
2021-06-25 14:31:27 +0500

    Setting up Video model

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -5,24 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Channel extends Model
+class Video extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
     public function getRouteKeyName()
     {
-        return 'slug';
+        return 'uid';
     }
 
-    public function videos(){
-        return $this->hasMany(Video::class);
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
     }
-
 }
